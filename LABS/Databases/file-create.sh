81f8c16fb26444aef1ec9e38b542503f642706d8
Sephens <adesephens@gmail.com>
2025-04-08 23:39:26 +0300

    creating file names

---
+++ LABS/Databases/file-create.sh
@@ -0,0 +1,10 @@
+touch "268-[DF]-Lab - Database Table Operations.md" \
+"269-[DF]-Lab - Insert, Update, and Delete Data in a Database.md" \
+"270-[DF]-Lab - Selecting Data from a Database.md" \
+"271-[DF]-Lab - Performing a Conditional Search.md" \
+"272-[DF]-Lab - Working with Functions.md" \
+"273-[DF]-Lab - Organizing Data.md" \
+"160-[DF]-Lab - Build Your Database Server and Interact with Your DB Using an App.md" \
+"274-[DF]-Lab - Introduction to Amazon Aurora.md" \
+"275-[DF]-Lab - Introduction to Amazon DynamoDB.md" \
+"162-[DF]-Lab - [Challenge] Build and Access an RDS Server.md"
